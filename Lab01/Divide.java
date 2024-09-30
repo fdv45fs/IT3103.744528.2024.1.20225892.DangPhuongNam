2de0ed54d97edf2f91579682cb792495a6f24bfa
fdv45fs <67596710+fdv45fs@users.noreply.github.com>
2024-09-30 10:46:43 +0700

    Added Divide program

---
+++ Lab01/Divide.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Divide {
+    public static void main(String[] args) {
+        String strNum1 = JOptionPane.showInputDialog("Enter the first number:");
+        String strNum2 = JOptionPane.showInputDialog("Enter the second number:");
+
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1*num2;
+
+        String text = "Sum: "+sum +"\n"+ "Difference: " + difference + "\n" + "Product: " + product + "\n";
+
+        if (num2 != 0) {
+            double quotient = num1/num2;
+            text += "Quotient: " + quotient;
+        } else {
+            text += "Cannot divide by zero";
+        }
+        JOptionPane.showMessageDialog(null, text);
+    }
+}
